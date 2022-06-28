c4392af8ad352ab2161e489dade4911fb18a4724
admin <you@example.com>
2022-06-28 14:33:10 +0200

    client test

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+import FileBase64 from 'react-file-base64'
+import {useState} from 'react'
+
+function App() {
+  const [image, setImage] = useState("");
+
+
+  return (
+    <div className="App">
+      <form action="http://localhost:5000/signup" method="post">
+        <h3>First Name</h3>
+          <input type="text" name="firstName" />
+        <h3>Email</h3>
+          <input type="email" name='email' />
+        <h3>Password</h3>
+          <input type="password" name='password' />
+          <br></br>
+          <br></br>
+          <input type="submit" value="Login" />
+      </form>
+        <h3>Please insert an image</h3>
+      <FileBase64 
+         multiple={false}
+         onDone={({ base64 }) => {
+           setImage(base64);
+         }}
+      />
+
+    </div>
+  );
+}
+
+export default App;
