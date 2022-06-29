7a6da21a6b50e9023dc3e24602b8f148a72ef969
WojtekU <voytecu@gmail.com>
2022-06-29 11:41:34 +0200

    devided to components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,18 @@
 import './App.css';
 import Signup from './components/Signup';
 import Upload from './components/Upload';
+import GetVids from './components/GetVids';
 
 function App() {
 
-const fetchVideos = () => {
-  fetch('http://localhost:5000/get-videos')
-  .then(res=>res.json())
-  .then(console.log)
-  .catch(console.log);
-}
 
   return (
     <div className="App">
       
       <Signup />
       <Upload />
-        
-
-
-    <button onClick={fetchVideos}>FetchALL</button>
-
-    <form action="http://localhost:5000/get-video" method="post">
-      <input type="text" name="fileName" /> <input type="submit" value="fetchOne"/>
-    </form>
+      <GetVids />
     
-
     </div>
     
   );
