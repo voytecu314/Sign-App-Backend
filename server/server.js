41fbeda12b7f2ba4d879c652e91d00004b592ca4
admin <you@example.com>
2022-06-29 11:20:48 +0200

    overwritting

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({limit: '100mb', extended: true}));
+app.use(express.urlencoded({limit: '100mb', extended: true}));
 app.use(cors());
 app.use('/', loginRoutes);
 app.use('/', filesRoutes);
